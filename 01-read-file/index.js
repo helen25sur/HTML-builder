0a2c8239105c7882790d90eb70e04974ffb660a7
Olena Surilova <elen21surilova@gmail.com>
2022-05-21 22:42:41 +0300

    feat: add code for the first task - 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', function (error, data) {
+  if (error) throw error;
+  console.log(data.toString());
+});

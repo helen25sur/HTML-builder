c5a17fdaa6889646788db60c01db88b75c9da9bf
Olena Surilova <elen21surilova@gmail.com>
2022-05-21 22:55:59 +0300

    fix: change code on the createReadStream in the first task - 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,18 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', function (error, data) {
+//   if (error) throw error;
+//   console.log(data.toString());
+// });
+
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', function (error, data) {
-  if (error) throw error;
-  console.log(data.toString());
+const rr = fs.createReadStream(path.join(__dirname, 'text.txt'));
+rr.on('readable', () => {
+  console.log(`readable: ${rr.read()}`);
+});
+rr.on('end', () => {
+  console.log('The reading ended');
 });

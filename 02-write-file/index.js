2d0ede39ffb1ceca5935475abb9a7cfea26abec5
Olena Surilova <elen21surilova@gmail.com>
2022-05-22 15:20:05 +0300

    feat: write code for the 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,13 +1,35 @@
 const fs = require('fs');
 const path = require('path');
-// const process = require('process');
-const readline = require('readline/promises');
+const process = require('process');
+const {
+  stdin
+} = require('process');
+const pathFile = path.join(__dirname, 'write.txt');
 
-const writeStr = fs.createWriteStream(path.join(__dirname, 'write.txt'), 'utf-8');
-writeStr.on('writable', () => {
-  console.log(`Please, enter your message: ${writeStr.write()}`);
-  // readline
+fs.writeFile(pathFile, '', () => {
+  console.log('File write.txt created');
+  const steam = fs.createWriteStream(pathFile, 'utf-8');
+  steam.on('open', () => {
+    console.log('Hello! Enter your message!');
+
+    stdin.on('data', data => {
+      if (data.toString().trim() === 'exit') {
+        console.log('Thanks! Have a nice day!');
+        steam.end();
+        process.exit();
+      }
+      console.log(`You typed ${data.toString()}`);
+      fs.appendFile(pathFile, data.toString(), () => {
+        console.log('Your message wrote in the file write.txt');
+      });
+
+    });
+  });
+  steam.on('end', () => {
+    steam.end();
+  });
 });
-writeStr.on('writableEnded', () => {
-  console.log('Thank you! Have a nice day!');
+process.on('SIGINT', () => {
+  console.log('Thanks! Have a nice day!');
+  process.exit();
 });
